70e138f8963248f0e0bb1b10cd8003a1b6c46614
Alvaro Costa Neto <nepheus.br@gmail.com>
2013-12-02 21:37:00 -0200

    Melhorei a organização do código com base em orientação a objetos.

--- letroquis.rb
+++ letroquis.rb
@@ -50,10 +50,9 @@ class Trie
 	end
 
 	def add(word)
-		return if word.empty?
 		node = @root
 		word.each_char { |char| node = node.add char }
-		node.valid = true
+		node.valid = node != @root
 	end
   
 	def traverse(node, &block)
@@ -68,10 +67,14 @@ class Trie
   def words(range = nil)
     result = []
     each_node do |node|
-      in_range = !range || (range && range === node.word.length)
-      result.push node.word if node.valid? && in_range
+      range_ok = !range || range === node.word.length
+      result.push node.word if node.valid? && range_ok
     end
     result
+  end
+  
+  def words=(list)
+    list.each { |word| add word.chomp }
   end
 
 	def fetch(word)
@@ -96,59 +99,68 @@ class Trie
 	end
 
 	def to_s
-		words.to_s
+		words.join ", "
 	end
 end
 
-@selected = Array.new
-@catalog = Trie.new
+class Game
+  MAX_COLUMNS = 80
+  
+  def initialize(filename)
+    # Preenche o catálogo de palavras disponíveis em uma árvore Trie
+    @catalog = Trie.new
+    @catalog.words = File.readlines filename
+    # As listas de palavras do jogo
+    @selected = []
+    @found = []
+    @current_word = @catalog.words(4..7).sample
+    @shuffled_word = @current_word.chars.shuffle.join
+    @word = ""
+  end
 
-# Este método recursivo obtém todos as palavras válidas do catálogo usando como base as letras da palavra escolhida
-def combine(word, letters)
-	node = @catalog[word]
-	# Se a palavra passada como parâmetro não gerar um caminho válido na árvore trie, qualquer cadeia descendente dela
-	# também será inválida
-	return if node == nil
-	@selected.push word if node.valid? && @selected.include?(word) == false
-	letters.each_with_index do |letter, index|
-		new_letters = Array.new(letters)
-		new_letters.delete_at index
-		combine(word + letter, new_letters)
-	end
-end
-
-File.readlines("palavras.txt").each do |line|
-	@catalog.add line.chomp
-end
-@current_word = @catalog.words(4..6).sample
-combine("", @current_word.chars)
-@selected.sort!
-@found = []
-@shuffled_word = @current_word.chars.shuffle.join(" ")
-# Inicia a mecânica do jogo
-word = ""
-while @found.length < @selected.length && word != "sair!"
-  # Imprime o tabuleiro
-  cheat = word == "cacildis!"
-  columns = 0
-  @selected.each do |word|
-    columns += word.length + 1
-    if columns >= 80
-      print "\n"
-      columns = 0
-    end
-  	if cheat || @found.include?(word)
-  		print "#{word} "
-  	else
-  		print ("#" * word.length) + " "
+  # Este método recursivo obtém todos as palavras válidas do catálogo usando como base as letras da palavra escolhida
+  def combine(word, letters)
+  	node = @catalog[word]
+  	# Se a palavra passada como parâmetro não gerar um caminho válido na árvore trie, qualquer cadeia descendente dela
+  	# também será inválida
+  	return if node == nil
+  	@selected.push word if node.valid?
+  	letters.each_with_index do |letter, index|
+  		new_letters = Array.new(letters)
+  		new_letters.delete_at index
+  		combine word + letter, new_letters
   	end
   end
-  if cheat
-    print "\n#{@current_word.chars.join(" ")}: "
-  else
-    print "\n#{@shuffled_word}: "
+  
+  def print_board
+    cheat = @word == "cacildis!"
+    output = ""
+    @selected.each do |word|
+      if output.length + word.length >= MAX_COLUMNS
+        print "#{output}\n"
+        output.clear
+      end
+    	if cheat || @found.include?(word)
+    		output << word
+    	else
+    		output << "#" * word.length
+    	end
+      output << " "
+    end
+    print "#{cheat ? @current_word : @shuffled_word}: "
   end
-	word = gets.chomp
-	@found << word if @selected.include? word
-  print "\n"
+  
+  def run
+    combine "", @current_word.chars
+    @selected.sort!.uniq!
+    while @found.length < @selected.length && @word != "sair!"
+      # Imprime o tabuleiro
+      print_board
+    	@word = gets.chomp
+    	@found.push @word if @selected.include? @word
+    end
+  end
 end
+
+game = Game.new "palavras.txt"
+game.run
